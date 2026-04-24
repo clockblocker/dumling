990a780ea911fe43916f1df1645e1aad74e82298
clockblocker <asde3211q@gmail.com>
2026-04-24 19:54:46 +0200

    init foulder

---
+++ docs-site/src/content/attestations-to-generate/de/selection/Sieh_einmal_hier_steht_er_pfui_der_Struwwelpeter/[Sieh]_einmal_hier_steht_er_pfui_der_Struwwelpeter.ts
@@ -0,0 +1,14 @@
+
+
+import type { AttestedSelection, Selection } from "dumling/types";
+
+const deSelection = {
+	
+} satisfies Selection;
+
+export const attestation = {
+	selection: deSelection,
+	sentenceMarkdown: `Sieh einmal, hier steht er, 
+pfui, der Struwwelpeter!`,
+	classifierNotes: "",
+} as const satisfies AttestedSelection;
