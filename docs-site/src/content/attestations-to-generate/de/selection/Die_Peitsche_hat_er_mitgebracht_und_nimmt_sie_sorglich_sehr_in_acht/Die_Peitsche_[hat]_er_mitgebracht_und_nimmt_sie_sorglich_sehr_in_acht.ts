9b11801058e721ca0d2ab1e057c9033ee9571400
clockblocker <asde3211q@gmail.com>
2026-04-24 20:03:20 +0200

    Add new attestation sentence folders

---
+++ docs-site/src/content/attestations-to-generate/de/selection/Die_Peitsche_hat_er_mitgebracht_und_nimmt_sie_sorglich_sehr_in_acht/Die_Peitsche_[hat]_er_mitgebracht_und_nimmt_sie_sorglich_sehr_in_acht.ts
@@ -0,0 +1,12 @@
+import type { AttestedSelection, Selection } from "dumling/types";
+
+const deSelection = {
+	
+} satisfies Selection;
+
+export const attestation = {
+	selection: deSelection,
+	sentenceMarkdown: `Die Peitsche [hat] er mitgebracht
+und nimmt sie sorglich sehr in acht.`,
+	classifierNotes: "",
+} as const satisfies AttestedSelection;
---
+++ docs-site/src/content/attestations-to-generate/de/selection/Es_brennt_die_Hand_es_brennt_das_Haar_es_brennt_das_ganze_Kind_sogar/Es_brennt_die_Hand_es_brennt_das_Haar_es_brennt_das_ganze_Kind_[sogar].ts
@@ -0,0 +1,12 @@
+import type { AttestedSelection, Selection } from "dumling/types";
+
+const deSelection = {
+	
+} satisfies Selection;
+
+export const attestation = {
+	selection: deSelection,
+	sentenceMarkdown: `Es brennt die Hand, es brennt das Haar,
+es brennt das ganze Kind [sogar].`,
+	classifierNotes: "",
+} as const satisfies AttestedSelection;
