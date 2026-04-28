1bf6bfeb57fc5a23d6cab68353f27bf20e858024
clockblocker <asde3211q@gmail.com>
2026-04-28 15:30:53 +0200

    Align German feature docs with attested UD coverage

--- docs-site/src/to-generate/docs/lang/de/feature/reflex.doc.ts
+++ docs-site/src/to-generate/docs/lang/de/feature/reflex.doc.ts
@@ -1,12 +1,14 @@
 import { defineLanguageOverlayPage } from "../../../../../lib/docs/source-mirrored-doc-pages.ts";
+import { attestation as dichReflexivePronoun } from "../../../../attestations/de/selection/Pass_auf_dich_auf/Pass_auf_[dich]_auf.ts";
 
 const document = defineLanguageOverlayPage({
-  description: "German Reflex.",
-  family: "feature",
-  leaf: "Reflex",
-  order: 8037,
-  subject: "Reflex",
-  title: "Reflex",
+	description: "German Reflex.",
+	examples: [dichReflexivePronoun],
+	family: "feature",
+	leaf: "Reflex",
+	order: 8037,
+	subject: "Reflex",
+	title: "Reflex",
 });
 
 export default document;
