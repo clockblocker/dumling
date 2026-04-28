dbe254c85ba1a63f39d2658855a72fc1a3512dff
clockblocker <asde3211q@gmail.com>
2026-04-28 15:05:11 +0200

    next batch

--- docs-site/src/to-generate/docs/lang/de/feature/conj-type.doc.ts
+++ docs-site/src/to-generate/docs/lang/de/feature/conj-type.doc.ts
@@ -1,12 +1,17 @@
 import { defineLanguageOverlayPage } from "../../../../../lib/docs/source-mirrored-doc-pages.ts";
 
 const document = defineLanguageOverlayPage({
-  description: "German ConjType.",
-  family: "feature",
-  leaf: "ConjType",
-  order: 8013,
-  subject: "ConjType",
-  title: "ConjType",
+	description: "German ConjType.",
+	family: "feature",
+	leaf: "ConjType",
+	order: 8013,
+	subject: "ConjType",
+	title: "ConjType",
+	body: `
+German currently exposes \`conjType\` as a lemma-level inherent feature on conjunction lexemes.
+
+At the moment, the public German schemas only allow \`conjType: "Comp"\`. The abstract enum also contains \`Oper\`, but that value is not currently used in the German pack.
+`,
 });
 
 export default document;
